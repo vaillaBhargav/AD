7cd66aac3082cf16d2a86628cae1ab841ba22336
2311cs020674@mallareddyuniversity.as.in <2311cs020674@mallareddyuniversity.ac.in>
2025-01-07 21:28:52 +0530

    Add files via upload

---
+++ day4.py.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def sum_of_evens(n):
+    if n <= 0:
+        return 
+    even_sum = sum(i for i in range(2, n + 1, 2))
+    return even_sum
+n = 10  
+result = sum_of_evens(n)
+print(f"The sum of all even numbers between 1 and {n} is: {result}")
+
+
+
+
+
+
+
+
+
+# In[ ]:
+
+
+
+
