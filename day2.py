bd0919717abf826ba5dd5eac97290907b7bbd65c
2311cs020674@mallareddyuniversity.as.in <2311cs020674@mallareddyuniversity.ac.in>
2025-01-07 16:33:37 +0000

    day-3

---
+++ day2.py
@@ -0,0 +1,12 @@
+#list
+list= [10, 20, 30, 40, 50]
+print(list[1])  
+print(list[-1])  
+#tuple
+tuple = ('apple', 'banana', 'cherry', 'date', 'fig')
+print(tuple[0])  
+print(tuple[3])  
+#dict
+dict = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5}
+print(dict['c']) 
+print(dict.get('e'))  
